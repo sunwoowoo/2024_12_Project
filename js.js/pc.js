9653303171e1b026f930d1a20504945f2246fbd6
DESKTOP-8Q87KGM\human-21 <gksmf2401@naver.com>
2024-12-11 13:41:53 +0900

    맹하늘
    수정1

---
+++ js.js/pc.js
@@ -0,0 +1,31 @@
+function scrollToElement1() {
+  document.getElementById('targetElement1').scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+  });
+}
+
+function scrollToElement2() {
+  document.getElementById('targetElement2').scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+  });
+}
+
+function scrollToElement3() {
+  document.getElementById('targetElement3').scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+  });
+}
+
+function scrollToElement4() {
+  document.getElementById('targetElement4').scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+  });
+}
+
+function changeImg() {
+  document.getElementById("img1").src = "./images/1.jpg";
+}
